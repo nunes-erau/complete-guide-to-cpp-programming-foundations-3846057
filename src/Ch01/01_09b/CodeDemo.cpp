85f3a8649c3458bdf0d74209b3e98493392a78e1
nunes-erau <nunessteffan@gmail.com>
2025-11-20 22:32:29 +0000

    Adding extra text for printing to the screen

--- src/Ch01/01_09b/CodeDemo.cpp
+++ src/Ch01/01_09b/CodeDemo.cpp
@@ -3,9 +3,13 @@
 // Terminal Interaction, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
 
 int main(){
-    std::cout << "Hi There!" << std::endl;
+    std::string name;
+    std::cout << "Enter your name:" << std::flush;
+    std::cin >> name; // cin only works for single words - for words with spaces there is another function for this
+    std::cout << "Name entered: " << name << "! " << std::endl;
 
     std::cout << std::endl << std::endl;
     return 0;
