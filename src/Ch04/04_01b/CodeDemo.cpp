37ca876aca0e5e8977b7f945aeb2eb7e04b3e56f
nunes-erau <nunessteffan@gmail.com>
2025-11-21 21:56:22 +0000

    Adding arrays

--- src/Ch04/04_01b/CodeDemo.cpp
+++ src/Ch04/04_01b/CodeDemo.cpp
@@ -4,8 +4,32 @@
 
 #include <iostream>
 
+//#define LEVEL_COUNT 4
+
 int main(){
+    const size_t LEVEL_COUNT = 4;
+
+    int enemies[LEVEL_COUNT];
+    float levelDifficulty[] = {1.0, 3.5, 6.0, 10.5};
+
+    enemies[0] = 10;
+    enemies[1] = 15;
+    enemies[2] = 20;
+    enemies[3] = 25;
     
+    std::cout << "enemies[0] = " << enemies[0] << std::endl;
+    std::cout << "enemies[1] = " << enemies[1] << std::endl;
+    std::cout << "enemies[2] = " << enemies[2] << std::endl;
+    std::cout << "enemies[3] = " << enemies[3] << std::endl;
+
+    std::cout << std::endl << std::endl;
+
+    std::cout << "levelDifficulty[0] = " << levelDifficulty[0] << std::endl;
+    std::cout << "levelDifficulty[1] = " << levelDifficulty[1] << std::endl;
+    std::cout << "levelDifficulty[2] = " << levelDifficulty[2] << std::endl;
+    std::cout << "levelDifficulty[3] = " << levelDifficulty[3] << std::endl;   
+
+
     std::cout << std::endl << std::endl;
     return 0;
 }
