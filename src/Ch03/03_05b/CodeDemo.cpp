d738d171f35194432099dbf3b739309d9eb7c32d
nunes-erau <nunessteffan@gmail.com>
2025-11-21 21:13:29 +0000

    Adding compound operators to the file

--- src/Ch03/03_05b/CodeDemo.cpp
+++ src/Ch03/03_05b/CodeDemo.cpp
@@ -12,14 +12,24 @@ int main(){
     std::cout << "Initial Zombies Incoming: " << incoming_zombies << std::endl;
 
     // Addition assignment
+    ammo += 10;
+    std::cout << "New ammo after addition: " << ammo << std::endl;
 
     // Subtraction assignment
+    ammo -= 5;
+    std::cout << "New ammo after subtraction: " << ammo << std::endl;
 
     // Multiplication assignment
+    incoming_zombies *=  2;
+    std::cout << "Zombies duplicated after timeout: " << incoming_zombies << std::endl;
 
     // Division assignment
+    ammo /= 2;
+    std::cout << "Ammo after division: " << ammo << std::endl;
 
     // Remainder assignment
+    ammo %= 6;
+    std::cout << "Ammo left after: " << ammo << std::endl;
 
     std::cout << std::endl << std::endl;
     return 0;
