e5e70811a391a5538d644c9a90bb3816a4c9b7b4
nunes-erau <nunessteffan@gmail.com>
2025-12-02 01:36:30 +0000

    Added vector class

--- src/Ch04/04_06b/CodeDemo.cpp
+++ src/Ch04/04_06b/CodeDemo.cpp
@@ -2,10 +2,19 @@
 // Exercise 04_06
 // The Vector Class, by Eduardo Corpeño 
 
+# include <vector>
 #include <iostream>
 #include <string>
 
 int main(){
+
+    std::vector<std::string> checkpoints = {"Matthew", "Mark", "Luke"};
+
+    checkpoints.push_back("John");
+
+    std::cout << "The number of checkpoints are: " << checkpoints.size() << std::endl;
+
+    std::cout << "Checkpoint 3 is " << checkpoints[2] << std::endl;
     
     std::cout << std::endl << std::endl;
     return 0;
